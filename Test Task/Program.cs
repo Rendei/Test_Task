394ce9c1b1bfb8d2e97f2ff0b9b3b1210684c710
Arseniy <cenz1217@gmail.com>
2024-04-11 00:04:49 +0500

    Добавьте файлы проекта.

---
+++ Test Task/Program.cs
@@ -0,0 +1,37 @@
+﻿using Serilog.Sinks;
+using Serilog.Events;
+using System.IO;
+using Serilog;
+
+namespace Test_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Запуск программы...");
+                string query = SqlFunctions.ReadQueryFromFile("input.txt");
+                string connectionString = SqlFunctions.ReadConnectionStringFromConfig();
+
+                SqlFunctions.ExecuteQueryAndWriteToFile(query, connectionString);
+                Log.Information("Запрос успешно выполнен. Результаты записаны в файл output.csv");
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Ошибка: {ex.Message}");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+    }
+}
---
+++ Test Task/SqlFunctions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Serilog;
+
+namespace Test_Task
+{
+    internal class SqlFunctions
+    {
+        public static string ReadConnectionStringFromConfig()
+        {
+            string connectionString = "";
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            connectionString = connectionString.Replace("|DataDirectory|", baseDirectory);
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ConfigurationErrorsException("Строка подключения не найдена в файле конфигурации.");
+            }
+
+            return connectionString;
+        }
+
+        public static string ReadQueryFromFile(string filePath)
+        {
+            string query = "";
+
+            if (File.Exists(filePath))
+            {
+                query = File.ReadAllText(filePath);
+            }
+            else
+            {
+                throw new FileNotFoundException("Файл с SQL запросом не найден");
+            }
+
+            return query;
+        }
+
+        public static void ExecuteQueryAndWriteToFile(string query, string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    Log.Debug($"Выполнение SQL запроса: {query}");
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        using (StreamWriter writer = new StreamWriter("output.csv"))
+                        {
+                            // Запись заголовков столбцов таблицы
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                writer.Write(reader.GetName(i));
+                                if (i < reader.FieldCount - 1)
+                                    writer.Write(",");
+                            }
+                            writer.WriteLine();
+
+                            //Запись данных из таблицы
+                            while (reader.Read())
+                            {
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    writer.Write(reader[i]);
+                                    if (i < reader.FieldCount - 1)
+                                        writer.Write(",");
+                                }
+                                writer.WriteLine();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
